2e56582d5482ffd3e423075ceffbce0332d986c8
natalykr <62375047+natalykr@users.noreply.github.com>
2025-03-26 17:44:07 +0300

    --added connection to PG

--- database.py
+++ database.py
@@ -2,11 +2,25 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, Mapped
 from sqlalchemy.testing.schema import mapped_column
 
+from config import settings
 
-engine = create_async_engine(
-    "sqlite+aiosqlite:///tasks.db",
+# # Синхронное подключение
+# sync_engine = create_engine(
+#     url=settings.DATABASE_URL_psycopg,
+#     echo=True,                              # Запросы к БД будут выводиться в консоль
+#     pool_size=5,                            # Лимит подключений
+#     max_overflow=10                         # Количество дополнительных подключений
+# )
+
+# Асинхронное подключение
+async_engine = create_async_engine(
+    url=settings.DATABASE_URL_asyncpg,
+    echo=True,                              # Запросы к БД будут выводиться в консоль
+    pool_size=5,                            # Лимит подключений
+    max_overflow=10                         # Количество дополнительных подключений
 )
-new_session = async_sessionmaker(engine, expire_on_commit=False)
+
+new_session = async_sessionmaker(async_engine, expire_on_commit=False)
 
 class Model(DeclarativeBase):
     pass
@@ -20,9 +34,9 @@ class TaskOrm(Model):
 
 
 async def create_tables():
-    async with engine.begin() as conn:
-        await conn.run_sync(Model.metadata.create_all)
+    async with async_engine.begin() as connection:
+        await connection.run_sync(Model.metadata.create_all)
 
 async def drop_tables():
-    async with engine.begin() as conn:
-        await conn.run_sync(Model.metadata.drop_all)
+    async with async_engine.begin() as connection:
+        await connection.run_sync(Model.metadata.drop_all)
