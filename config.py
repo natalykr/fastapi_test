0fd3b8b800d2b5c60a21da4e0dea495d36c57e63
natalykr <62375047+natalykr@users.noreply.github.com>
2025-03-26 18:04:07 +0300

    --fixed config.py

--- config.py
+++ config.py
@@ -4,16 +4,16 @@ class Settings(BaseSettings):
     PG_HOST: str
     PG_PORT: str
     PG_USER: str
-    PG_PASSWORD: str
+    PG_PASS: str
     PG_NAME: str
 
     @property
     def DATABASE_URL_asyncpg(self):
-        return f"postgresql+asyncpg://{self.PG_USER}:{self.PG_PASSWORD}@{self.PG_HOST}:{self.PG_PORT}/{self.PG_NAME}"
+        return f"postgresql+asyncpg://{self.PG_USER}:{self.PG_PASS}@{self.PG_HOST}:{self.PG_PORT}/{self.PG_NAME}"
 
     @property
     def DATABASE_URL_psycopg(self):
-        return f"postgresql+psycopg://{self.PG_USER}:{self.PG_PASSWORD}@{self.PG_HOST}:{self.PG_PORT}/{self.PG_NAME}"
+        return f"postgresql+psycopg://{self.PG_USER}:{self.PG_PASS}@{self.PG_HOST}:{self.PG_PORT}/{self.PG_NAME}"
 
     model_config = SettingsConfigDict(env_file=".env")
 
